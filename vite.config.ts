bec9aec2152c000ccf034e983d2030eda6a4fa8e
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-12-23 01:52:59 +0900

    use hash

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
-  },
 });
