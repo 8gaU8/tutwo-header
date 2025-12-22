04427011003ce6965bf4b1aa9d735cf01a45477d
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-12-23 01:50:49 +0900

    update dist

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import { Canvas, type ThreeElements, useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
 import * as THREE from "three";
+import { FrontSide } from "three";
+
+// import "./main.css";
 
 export function Box(props: ThreeElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!);
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
-  useFrame((_state, delta) => (ref.current.rotation.x += delta));
+  useFrame((_state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta * 0.8;
+    ref.current.rotation.z += delta * 0.5;
+  });
   return (
     <mesh
       {...props}
@@ -17,24 +24,51 @@ export function Box(props: ThreeElements["mesh"]) {
       onPointerOver={() => hover(true)}
       onPointerOut={() => hover(false)}
     >
-      <boxGeometry args={[3, 3, 3]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      <torusKnotGeometry args={[2, 0.5, 128, 64]} />
+      {/* <meshPhysicalMaterial
+        // color={hovered ? "hotpink" : "orange"}
+        // side={THREE.DoubleSide}
+        // roughness={0.2}
+        // iridescence={1}
+        // iridescenceIOR={1}
+        // iridescenceThicknessRange={[100, 800]}
+        // transmission={0.5}
+        // ior={1.2}
+        // thickness={1}
+        // metalness={0}
+        // roughness={0}
+      /> */}
+      <meshStandardMaterial
+        color={hovered ? "hotpink" : "green"}
+        side={FrontSide}
+        roughness={0.8}
+        metalness={0.5}
+        transparent={true}
+
+        opacity={0.9}
+      />
+      {/* <MeshTransmissionMaterial
+        color={hovered ? "hotpink" : "orange"}
+        side={THREE.DoubleSide}
+        backsideThickness={1.5}
+        thickness={hovered ? 5 : 2}
+        transmission={0.5}
+      /> */}
     </mesh>
   );
 }
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <Canvas>
+  <Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
     <ambientLight intensity={Math.PI / 2} />
     <spotLight
       position={[10, 10, 10]}
       angle={0.15}
       penumbra={1}
       decay={0}
-      intensity={Math.PI}
+      intensity={3}
     />
     <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-    <Box position={[-2, 0, 0]} />
-    <Box position={[2, 0, 0]} />
+    <Box position={[0, 0, 0]} />
   </Canvas>
 );
